162943c834d817e07d1b1da61de3b73fb772b869
shakil89427 <shakilahmed89427@gmail.com>
2022-12-29 16:42:08 +0600

    checked

--- index.js
+++ index.js
@@ -11,15 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (_, res) => res.send("Server running"));
 app.post("/", (req, res) => {
   try {
-    const { authData, url, waitFrom, waitTill } = req.body;
-    if (!authData || !url || !waitFrom || !waitTill) {
+    const { auth, url, waitFrom, waitTill } = req.body;
+    if (!auth || !url || !waitFrom || !waitTill) {
       return res.send("invalid params");
     }
-    if (
-      !Array.isArray(authData) ||
-      typeof waitFrom !== "number" ||
-      typeof waitTill !== "number"
-    ) {
+    if (typeof waitFrom !== "number" || typeof waitTill !== "number") {
       return res.send("invalid params");
     }
     if (waitFrom > waitTill) {
@@ -28,14 +24,12 @@ app.post("/", (req, res) => {
     const waitTime = () => {
       return Math.floor(Math.random() * (waitTill - waitFrom + 1)) + waitFrom;
     };
-    authData.forEach((auth) => {
-      const authUri = `http://${auth}@hub-cloud.browserstack.com/wd/hub`;
-      runTest(createBrowser(1), authUri, url, waitTime());
-      runTest(createBrowser(2), authUri, url, waitTime());
-      runTest(createBrowser(3), authUri, url, waitTime());
-      runTest(createBrowser(4), authUri, url, waitTime());
-      runTest(createBrowser(5), authUri, url, waitTime());
-    });
+    const authUri = `http://${auth}@hub-cloud.browserstack.com/wd/hub`;
+    runTest(createBrowser(1), authUri, url, waitTime());
+    runTest(createBrowser(2), authUri, url, waitTime());
+    runTest(createBrowser(3), authUri, url, waitTime());
+    runTest(createBrowser(4), authUri, url, waitTime());
+    runTest(createBrowser(5), authUri, url, waitTime());
     res.send("Command Started");
   } catch (error) {
     res.send("Something went wrong");
